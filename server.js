f56bebc6a71048d413be184b173d1a0f20cd9bbd
Prithviraj-Indulkar <rindulkar2003@gmail.com>
2024-11-11 14:43:48 +0530

    port env

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 dotenv.config();
 
 const app = express();
-const port = 8888;
+const port =process.env.PORT|| 8888;
 app.use(express.json());
 app.use(cors());
 mongoose
